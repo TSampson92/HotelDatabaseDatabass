a2adc3a39ddf124de57053988236d061e1915f41
Petra Power <petra.powered@gmail.com>
2018-11-16 16:46:36 -0800

    Created new branch, inserted Petra's info

--- HotelInserts.sql
+++ HotelInserts.sql
@@ -1,1 +1,2 @@
 INSERT INTO GUEST VALUES('torren.sampson@gmail.com', 'Sampson', '1111222233334444', '6199525179', '11920 98th Ave Kirkland, WA 98034');
+INSERT INTO GUEST VALUES('petra.power@gmail.com','Power','3333000033330000','123456789','Address Lane 8, Redmond, WA, 98000');
