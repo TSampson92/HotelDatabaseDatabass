8b2d425ab77b3286f50947440089c78d8a98fc79
Petra Power <petra.powered@gmail.com>
2018-11-16 16:39:04 -0800

    Added Petra's info

--- HotelInserts.sql
+++ HotelInserts.sql
@@ -1,1 +1,2 @@
 INSERT INTO GUEST VALUES('torren.sampson@gmail.com', 'Sampson', '1111222233334444', '6199525179', '11920 98th Ave Kirkland, WA 98034');
+INSERT INTO GUEST VALUES('petra.power@gmail.com','Power','3333000033330000','1234567890','Address Lane 8, Redmond, WA, 98074');
