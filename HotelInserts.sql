629af71221b6e6e45a346279ac129c5c0779f8fc
Ashley Hay <hayashley@msn.com>
2018-11-16 16:50:45 -0800

    added insert to guest table

--- HotelInserts.sql
+++ HotelInserts.sql
@@ -1,1 +1,2 @@
 INSERT INTO GUEST VALUES('torren.sampson@gmail.com', 'Sampson', '1111222233334444', '6199525179', '11920 98th Ave Kirkland, WA 98034');
+INSERT INTO GUEST VALUES('hayashley@gmail.com', 'Hay', '387569023567865', '206 878 9342', '13713 87th Ave Redmond, WA 98013');
