7a37e426f077d99ecf0fae41341dc536a5b3cdc8
Torren Sampson <tsampson2092@gmail.com>
2018-11-26 09:34:14 -0800

    Pulling Sarahs changes  (#12)
    * List of nearly all Harry Potter characters as guests.
    
    * Edited guest list by adding email addresses.
    
    * Removed some weird stuff from the top
    
    * Separated employees from guest list.
    
    * Removed employees from guest list.
    
    * Add conference room inserts.
    
    * I changed the weird smart quotes to standard ones, and also added in some missing quotes.
    
    * Add membership inserts.
    
    * Removed spaces.
    
    * Made our personal guest inserts more uniform.

--- HotelInserts.sql
+++ HotelInserts.sql
@@ -1,8 +1,7 @@
-INSERT INTO GUEST VALUES('torren.sampson@gmail.com', 'Sampson', '1111222233334444', '6199525179', '11920 98th Ave Kirkland, WA 98034');
+INSERT INTO GUEST VALUES('torren.sampson@gmail.com', 'Torren Sampson', '1111222233334444', '6199525179', '11920 98th Ave, Kirkland, WA 98034');
 
-INSERT INTO GUEST VALUES('srobbins8@frontier.com', 'Robbins', '1234123412341234', '12342342345', '12345 67th St Fairyland, WA 98296');
+INSERT INTO GUEST VALUES('srobbins8@frontier.com', 'Sarah Robbins', '1234123412341234', '12342342345', '12345 67th St, Fairyland, WA 98296');
 
-INSERT INTO GUEST VALUES('hayashley@gmail.com', 'Hay', '387569023567865', '206 878 9342', '13713 87th Ave Redmond, WA 98015');
+INSERT INTO GUEST VALUES('hayashley@gmail.com', 'Ashley Hay', '387569023567865', '2068789342', '13713 87th Ave, Redmond, WA 98015');
 
-INSERT INTO GUEST VALUES('petra.power@gmail.com','Power','3333000033330000','123456789','Address Lane 8, Redmond, WA, 98000');
-
+INSERT INTO GUEST VALUES('petra.power@gmail.com','Petra Power','3333000033330000','123456789','Address Lane 8, Redmond, WA 98000');
